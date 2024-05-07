c27804061f894168a1a4e231aa29bd3c6e741de7
Lucia <mihalikova.lucia@yahoo.com>
2024-05-07 20:20:50 +0200

    Updated members

--- main.py
+++ main.py
@@ -1,18 +1,64 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify
 
 app = Flask(__name__)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://u1ai5zsuzpe6y74uwyqu:0BacMZwIRVg5rpsOcndFG1pcHSUba5@by1wvfyyi5kgu1euhyko-postgresql.services.clever-cloud.com:50013/by1wvfyyi5kgu1euhyko"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 from authors import Author
-
-@app.route("/authors", methods=["GET"])
+from members import Members
+@app.route("/authors/", methods=['GET'])
 def getAuthors():
     authors = Author.query.all()
-    return "Authors"
+
+    author_list = [
+        {"author_id": author.author_id, "name": author.name, "bio": author.bio}
+        for author in authors
+    ]
+    #author_list = [aut.to_dict() for aut in authors] pri tejto moznosti treba dorobit funkciu to_dict do file author
+    return jsonify (author_list)
+
+# @app.route("/members/", methods=['GET'])
+#     members=Member.query.all()
+#
+#     member_list = [mem.to_dict() for mem in members] # pri tejto moznosti treba dorobit funkciu to_dict do file author
+#     return jsonify (member_list)
+
+@app.route("/members/", methods=['GET'])
+def getMembers():
+    members=Members.query.all()
+
+    member_list = [mem.to_dict() for mem in members] # pri tejto moznosti treba dorobit funkciu to_dict do file author
+    return jsonify(member_list)
+
+
+@app.route("/members/<int:user_id>", methods=["GET"])
+def getMember(user_id):
+    member = Members.query.get(str(user_id))
+    if member:
+        return jsonify(member.to_dict()), 200
+    else:
+        return "User not found", 404
+
+
+@app.route('/members', methods=['POST'])
+def registerUser():
+    data = request.json
+    first_name = data.get("first_name")
+    last_name = data.get("last_name")
+    email = data.get("email")
+    password = data.get("password")
+
+
+    createUser = Members.create_from_request(request)
+    return jsonify(createUser.to_dict()), 200
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
+
+
 
--- members.py
+++ members.py
@@ -1,12 +1,23 @@
 from main import db
 
 
-class Genres(db.Model):
+class Members(db.Model):
     __tablename__ = "members"
 
     member_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    email = db.Column(db.String(), unique=True)
-    password = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(225), nullable=False)
+    last_name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), unique=True)
     registration_date = db.Column(db.DateTime)
+    password = db.Column(db.String(255))
 
+
+    def to_dict(self):
+        return {
+            "member_id":self.member_id,
+            "first_name":self.first_name,
+            "last_name":self.last_name,
+            "email":self.email,
+            "registration_date":self.registration_date,
+            "password":self.password
+        }
